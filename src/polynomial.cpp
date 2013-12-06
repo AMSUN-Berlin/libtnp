49db2c881381900a8326323619d87c58f856ff40
Christoph Höger <christoph.hoeger@tu-berlin.de>
2013-12-06 02:07:13 +0100

    Polynomial class for Composition
    Signed-off-by: Christoph Höger <christoph.hoeger@tu-berlin.de>

---
+++ src/polynomial.cpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2012 uebb.tu-berlin.de.
+ *
+ * This file is part of tnp
+ *
+ * tnp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tnp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with tnp. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "polynomial.hpp"
+
+#include <math.h>
+
+namespace tnp {
+
+  using namespace boost::heap;
+
+  bool Term::operator <(const Term& o) const {
+    return o.monomial < monomial;
+  }
+
+  double Term::eval(const std::vector<double>& arg) const {
+    double res = factor;
+    for(auto e : monomial) {
+      res *= pow(arg[std::get<0>(e)], std::get<1>(e));
+    }
+    return res;
+  }
+
+  Term Term::operator^(unsigned int p) const {
+    Monomial m2(monomial);
+    for (auto e : m2)
+      std::get<1>(e) += p;
+    return Term(pow(factor, p), m2);
+  }
+
+  Term Term::operator*(const int f) const {
+    return Term(factor*f, monomial);
+  }
+
+  StdPolynomial Term::operator+(const Term& t) const {
+    if (t.monomial == monomial)
+      return StdPolynomial(Term(factor + t.factor, monomial));
+    else {
+      priority_queue<Term> ts;
+      ts.push(t);
+      ts.push(*this);
+      return StdPolynomial(ts);
+    }
+  }    
+
+  Term var(unsigned int idx) {
+    return Term(1, Monomial({{idx, 1}}));
+  }
+      
+  double StdPolynomial::eval(const std::vector<double>& arg) const {
+    double res = 0.0;
+    for (Term t : terms)
+      res += t.eval(arg);
+    return res;
+  }
+}
---
+++ src/polynomial.hpp
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2012 uebb.tu-berlin.de.
+ *
+ * This file is part of tnp
+ *
+ * tnp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * tnp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with tnp. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef POLYNOMIAL_HPP
+#define POLYNOMIAL_HPP 1
+
+#include <boost/heap/priority_queue.hpp>
+#include <utility>
+#include <map>
+
+namespace tnp {
+
+  using namespace boost::heap;
+
+  class StdPolynomial;
+
+  /**
+   * standard representation of polynomials
+   */
+  typedef std::map<unsigned int, unsigned int> Monomial;
+
+  class Term {
+  public:
+    int factor;
+    Monomial monomial;
+
+    Term(int f) : factor(f) {}
+
+    Term(int f, Monomial m) : factor(f), monomial(m) {}
+
+    inline double eval(const std::vector<double>& arg) const;
+
+    Term operator ^(unsigned int p) const;
+
+    bool operator <(const Term& o) const;
+    
+    Term operator*(const int f) const;
+
+    StdPolynomial operator+(const Term& t) const; 
+  };
+
+  Term var(unsigned int idx);
+
+  class StdPolynomial {
+  public:
+    priority_queue<Term> terms;
+
+    StdPolynomial(Term t) { terms.push(t); }
+
+    StdPolynomial(priority_queue<Term> t) : terms(t) {}
+
+    double eval(const std::vector<double>& arg) const;
+  };
+}
+
+#endif
