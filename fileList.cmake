8caf30cbf6e96edb3a5c823437c8697774649bd1
Christoph Höger <choeger@umpa-net.de>
2013-12-20 12:13:26 +0100

    Fix weird typo
    Signed-off-by: Christoph Höger <choeger@umpa-net.de>

--- fileList.cmake
+++ fileList.cmake
@@ -11,7 +11,6 @@ set(srcs ${srcs_dir}/tnp.cpp
          ${srcs_dir}/npnumber.cpp
 	 ${srcs_dir}/multiplication.cpp
 	 ${srcs_dir}/composition.cpp
-	 ${srcs_dir}/examples.cpp
 	 ${srcs_dir}/polynomial.cpp
   )
 
