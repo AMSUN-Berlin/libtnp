43acffbd69fce64490403d17f21cbccf0b09e2c8
Christoph Höger <christoph.hoeger@tu-berlin.de>
2014-04-10 17:59:13 +0200

    Install lib headers
    Signed-off-by: Christoph Höger <christoph.hoeger@tu-berlin.de>

--- fileList.cmake
+++ fileList.cmake
@@ -3,18 +3,25 @@ set(hdrs_dir ${${PROJECT_NAME}_include_dir})
 set(tests_dir ${CMAKE_CURRENT_SOURCE_DIR}/${test_dir})
 set(srcs_dir ${CMAKE_CURRENT_SOURCE_DIR}/${source_dir})
 
-#Project header files
-set(hdrs ${hdrs_dir}/tnp.hpp)
 
 #Project source files
-set(srcs ${srcs_dir}/tnp.cpp
-         ${srcs_dir}/npnumber.cpp
-	 ${srcs_dir}/multiplication.cpp
-	 ${srcs_dir}/composition.cpp
-	 ${srcs_dir}/polynomial.cpp
-	 ${srcs_dir}/ops.cpp
+set(${PROJECT_NAME}_srcs ${srcs_dir}/tnp.cpp
+                        ${srcs_dir}/npnumber.cpp
+			${srcs_dir}/multiplication.cpp
+			${srcs_dir}/composition.cpp
+			${srcs_dir}/polynomial.cpp
+			${srcs_dir}/ops.cpp
   )
 
 #Project tests
-set(test_sources ${tests_dir}/simpleTest.cpp ${tests_dir}/unaryAlgebraic.cpp)
+set(${PROJECT_NAME}_test_sources ${tests_dir}/simpleTest.cpp ${tests_dir}/unaryAlgebraic.cpp)
 
+#Public API
+set(${PROJECT_NAME}_headers ${hdrs_dir}/tnp.h
+			    ${hdrs_dir}/tnp.hpp
+                            ${hdrs_dir}/tnp/ops.h
+			    ${hdrs_dir}/tnp/npnumber.hpp
+			    ${hdrs_dir}/tnp/polynomial.hpp
+			    ${hdrs_dir}/tnp/ops/multiplication.hpp
+			    ${hdrs_dir}/tnp/ops/composition.hpp
+			    )
